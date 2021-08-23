f7f646794dc7f23e136f1210b21efadd6d32bf0a
GregNicholas <59461870+GregNicholas@users.noreply.github.com>
2021-08-23 11:26:54 -0700

    revert smooth scroll

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-import smoothscroll from 'smoothscroll-polyfill';
-
-// kick off the polyfill!
-smoothscroll.polyfill();
-
-
 window.addEventListener('scroll', function() {
     const header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
