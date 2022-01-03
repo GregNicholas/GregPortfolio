0c8eca32a21f15ac0a4b364029151e2523767e4d
GregNicholas <59461870+GregNicholas@users.noreply.github.com>
2022-01-03 09:31:37 -0800

    toggle project view

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-window.addEventListener('scroll', function() {
+const extraProjects = document.getElementsByClassName("extra-projects");
+const toggleButton = document.getElementById("project-toggle");
+
+window.addEventListener('scroll', function () {
     const header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 0);
 });
-
-// const toggleMenu = () => {
-//     const menuToggle = document.querySelector(".toggle");
-//     menuToggle.classList.toggle('active');
-// }
 
 const toggleMenu = () => {
     const menuToggle = document.getElementById("btnHamburger");
@@ -15,6 +13,19 @@ const toggleMenu = () => {
     menuToggle.classList.toggle('open');
 }
 
+const toggleProjectView = (e) => {
+    const len = extraProjects !== null ? extraProjects.length : 0
+    for (let i = 0; i < len; i++) {
+        extraProjects[i].classList.toggle("hidden-projects")
+    }
+
+    if (toggleButton.innerText === "VIEW MORE") {
+        toggleButton.innerText = "HIDE"
+    } else {
+        toggleButton.innerText = "VIEW MORE"
+    }
+}
+
 const date = new Date();
-    document.getElementById("copyright").innerHTML=`&#169 Gregory Schoenberg ${date.getFullYear()}`;
+document.getElementById("copyright").innerHTML = `&#169 Gregory Schoenberg ${date.getFullYear()}`;
 
